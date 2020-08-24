54ccf1f631c4f732e3dea30fe3a5647de1d4d444
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-24 15:55:14 +0800

    create : 切版和樣式1

--- create.php
+++ create.php
@@ -32,23 +32,32 @@
     <!-- 輪轉圖 -->
     <div id="banner"></div>
 
+    <!-- 創建表格 -->
+    <div id="formTitle">註冊會員</div>
+    <form action="" method="post">
+        <label for="newName">帳號</label>
+        <input type="text" name="newName" id="newName" placeholder="請輸入8~15位的英文和數字" pattern="\w{8,15}" required>
+        <label for="newPassword">密碼</label>
+        <input type="text" name="newPassword" id="newPassword" placeholder="請輸入8~15位的英文和數字" pattern="\w{8,15}" required>
+        <label for="newPasswordCheck">再輸入密碼</label>
+        <input type="text" name="newPasswordCheck" id="newPasswordCheck">
+        <label for="truthName">本名</label>
+        <input type="text" name="truthName" id="truthName" required>
+        <label for="phone">電話</label>
+        <input type="text" name="phone" id="phone" placeholder="範例: 0912345678" pattern="\d{10}" required>
+        <label for="email">電子信箱</label>
+        <input type="text" name="email" id="email" pattern="\w+([.-]\w+)*@\w+([.]\w+)+" required>
+        <label for="address">地址</label>
+        <input type="text" name="address" id="address" required>
+        
+        <input type="checkbox" name="check" id="check">
+        <label for="check">我同意本平台的使用者規範</label>
 
-    <!-- 登入區塊 -->
-    <div id="login">
-        <div id="loginInput">
-            <div id="image"></div>
-            <div id="text">
-                <form action="" method="POST">
-                    <label for="userName">帳號</label>
-                    <input type="text" name="userName" id="userName">
-                    <label for="userPassword">密碼</label>
-                    <input type="text" name="userPassword" id="userPassword">
-                    <input type="submit" value="送出" id="submit">
-                </form>
-                <div id="close"></div>
-            </div>
+        <div id="btnGroup">
+            <input type="submit" value="確認" class="button">
+            <input type="reset" value="重設" class="button">
         </div>
-    </div>
+    </form>
 
     <!-- 聯絡我們 -->
     <footer>
@@ -67,20 +76,6 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
-        $(document).ready(function (){
-            $("#login").hide();
-
-            $("#member").on("click", function(){
-                $("#login").show();
-            });
-
-            $("#close").on("click", function(){
-                $("#userName").val("");
-                $("#userPassword").val("");
-                $("#login").hide();
-            });
-        });
-
     </script>
 </body>
 </html>
