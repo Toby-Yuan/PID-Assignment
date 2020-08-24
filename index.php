a21c202b40a18f1ef36142dd40145389c78273c4
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-24 12:04:16 +0800

    index : about樣式1

--- index.php
+++ index.php
@@ -34,13 +34,13 @@
 
     <!-- 關於我們 -->
     <section id="about">
-        <img src="img/index_about.jpg" alt="關於我們">
+        <div id="image"></div>
         <div id="text">
             <h5>三點半 3.PI</h5>
             <p>
                 一個完整的派就是一個圓, 而一個圓也就是一個PI<br>
                 而PI也代表著3點, 正如我們的出爐時間<br>
-                為完美的午茶, 點綴完美的句號
+                為完美的午茶, 點綴完美的句點
             </p>
         </div>
     </section>
