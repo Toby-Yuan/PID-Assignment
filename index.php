b1f36d26647465ba6631732778b4ae51a82816bf
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-24 11:56:30 +0800

    index : about初切

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <!--  本頁面各連結 -->
             <div id="link">
                 <a href="">三點半</a>
-                <a href="">關於我們</a>
+                <a href="#about">關於我們</a>
                 <a href="">熱門商品</a>
                 <a href="">聯絡我們</a>
             </div>
@@ -31,5 +31,19 @@
 
     <!-- 輪轉圖 -->
     <div id="banner"></div>
+
+    <!-- 關於我們 -->
+    <section id="about">
+        <img src="img/index_about.jpg" alt="關於我們">
+        <div id="text">
+            <h5>三點半 3.PI</h5>
+            <p>
+                一個完整的派就是一個圓, 而一個圓也就是一個PI<br>
+                而PI也代表著3點, 正如我們的出爐時間<br>
+                為完美的午茶, 點綴完美的句號
+            </p>
+        </div>
+    </section>
+
 </body>
 </html>
