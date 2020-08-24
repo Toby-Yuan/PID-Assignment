48848fb0741f8641a977334c243cca536b406974
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-24 16:04:26 +0800

    購物車頁面建立以及連接樣式檔

---
+++ buyBus.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>三點半</title>
+
+    <link rel="stylesheet" href="./CSS/buyBusStyle.css">
+
+</head>
+<body>
+
+    <!-- 導覽列 -->
+    <nav>
+        <div id="box">
+
+            <!--  本頁面各連結 -->
+            <div id="link">
+                <a href="#">三點半</a>
+                <a href="#about">關於我們</a>
+                <a href="product.php">熱門商品</a>
+                <a href="#contact">聯絡我們</a>
+            </div>
+            <div></div>
+            <div id="member">
+                <a id="loginOpen">登入</a>
+            </div>
+
+        </div>
+    </nav>
+
+    <!-- 輪轉圖 -->
+    <div id="banner"></div>
+
+    <!-- 聯絡我們 -->
+    <footer>
+        <div id="contact">
+            <div id="logo">
+                <h2>三點半</h2>
+            </div>
+            <div id="content">
+                <p>
+                    地址: 台中市西屯區逢甲路20巷<br>
+                    聯絡電話: (04)4536782
+                </p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        
+    </script>
+</body>
+</html>
