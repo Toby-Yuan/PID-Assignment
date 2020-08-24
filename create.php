0c39f14d80d181c9460d9bb8f82a2b76289196db
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-24 15:21:41 +0800

    readme : 規劃顧客資料所需

---
+++ create.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>三點半</title>
+
+    <link rel="stylesheet" href="./CSS/createStyle.css">
+
+</head>
+<body>
+
+    <!-- 導覽列 -->
+    <nav>
+        <div id="box">
+
+            <!--  本頁面各連結 -->
+            <div id="link">
+                <a href="#">三點半</a>
+                <a href="#about">關於我們</a>
+                <a href="product.php">熱門商品</a>
+                <a href="#contact">聯絡我們</a>
+            </div>
+            <div></div>
+            <div id="member">
+                <a id="loginOpen">登入</a>
+            </div>
+
+        </div>
+    </nav>
+
+    <!-- 輪轉圖 -->
+    <div id="banner"></div>
+
+
+    <!-- 登入區塊 -->
+    <div id="login">
+        <div id="loginInput">
+            <div id="image"></div>
+            <div id="text">
+                <form action="" method="POST">
+                    <label for="userName">帳號</label>
+                    <input type="text" name="userName" id="userName">
+                    <label for="userPassword">密碼</label>
+                    <input type="text" name="userPassword" id="userPassword">
+                    <input type="submit" value="送出" id="submit">
+                </form>
+                <div id="close"></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- 聯絡我們 -->
+    <footer>
+        <div id="contact">
+            <div id="logo">
+                <h2>三點半</h2>
+            </div>
+            <div id="content">
+                <p>
+                    地址: 台中市西屯區逢甲路20巷<br>
+                    聯絡電話: (04)4536782
+                </p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function (){
+            $("#login").hide();
+
+            $("#member").on("click", function(){
+                $("#login").show();
+            });
+
+            $("#close").on("click", function(){
+                $("#userName").val("");
+                $("#userPassword").val("");
+                $("#login").hide();
+            });
+        });
+
+    </script>
+</body>
+</html>
