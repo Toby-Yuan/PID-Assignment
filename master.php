cccb43cb1f18138ec53aedf26eb7796000baf132
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-26 09:28:07 +0800

    建立管理者首頁以及連接樣式檔

---
+++ master.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>三點半 管理</title>
+    <link rel="stylesheet" href="CSS/masterStyle.css">
+</head>
+<body>
+    <nav>
+        <div id="box">
+            <h4>管理員: Dent0204</h4>
+            <a href="">商品列表</a>
+            <a href="">會員列表</a>
+            <a href="">訂單管理</a>
+            <div></div>
+        </div>
+    </nav>
+    
+</body>
+</html>
