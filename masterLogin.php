56c11fbaab077547c9443315a5e36d3424bf23f1
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-26 08:59:31 +0800

    建立管理員登入畫面以及連接樣式檔

---
+++ masterLogin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>三點半 管理</title>
+    <link rel="stylesheet" href="CSS/masterLoginStyle.css">
+</head>
+<body>
+    
+</body>
+</html>
