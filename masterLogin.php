8e21ebd26a1cf4a48d7430b5bc7e418422c2606e
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-26 09:17:21 +0800

    masterLogin : 初切+樣式1

--- masterLogin.php
+++ masterLogin.php
@@ -7,6 +7,23 @@
     <link rel="stylesheet" href="CSS/masterLoginStyle.css">
 </head>
 <body>
-    
+    <form action="" method="post">
+        <table>
+            <tr>
+                <th colspan="2">管理員登入</th>
+            </tr>
+            <tr>
+                <td class="labelText">帳號:</td>
+                <td><input type="text" name="masterName" id="masterName" required></td>
+            </tr>
+            <tr>
+                <td class="labelText">密碼:</td>
+                <td><input type="password" name="masterPassword" id="masterPassword" required></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="登入"></td>
+            </tr>
+        </table>
+    </form>
 </body>
 </html>
