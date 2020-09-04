f6d09f4fe87e9428927ff1e320f1de43f4a284b3
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-09-04 17:47:08 +0800

    MVC : masterPost MVC建立

---
+++ MVCmod/controllers/masterPostController.php
@@ -0,0 +1,124 @@
+<?php
+
+require_once './models/masterPostModel.php';
+
+class masterPostC {
+    public $result;
+
+    public function __construct(){
+        $this->result = new masterPostM();
+    }
+
+    public function chooseDay(){
+        if(isset($_POST["oneD"])){
+            return $this->show(1);
+        }
+
+        if(isset($_POST["senD"])){
+            return $this->show(7);
+        }
+
+        if(isset($_POST["oneM"])){
+            return $this->show(30);
+        }
+
+        if(isset($_POST["choose"])){
+            $date1 = $_POST["date1"];
+            $date2 = $_POST["date2"];
+            if($date1 < $date2){
+                $bigDate = $date2;
+                $smlDate = $date1;
+            }else{
+                $bigDate = $date1;
+                $smlDate = $date2;
+            }
+
+            return $this->showChoose($smlDate, $bigDate);
+        }
+    }
+
+    public function show($val){
+        $income = 0;
+        $product = $this->result->product();
+        $list = "";
+
+        foreach($product as $key=>$value){
+            $sallDemand = $this->result->demand($val, $value[0], $value[2]);
+
+            $one = <<<only
+            <tr>
+            <td>$value[1]</td>
+            <td>$sallDemand[0]</td>
+            <td>$value[4]</td>
+            <td>$value[2]</td>
+            <td>$sallDemand[1]</td>
+            </tr>
+            only;
+
+            $income += $sallDemand[1];
+            $list .= $one;
+        }
+        $list .= "<tr><td colspan='5'>總營收: $income</td></tr>";
+        return $list;
+    }
+
+    public function showChoose($day1, $day2){
+        $income = 0;
+        $product = $this->result->product();
+        $list = "";
+
+        foreach($product as $key=>$value){
+            $sallDemand = $this->result->demandDay($day1, $day2, $value[0], $value[2]);
+
+            $one = <<<only
+            <tr>
+            <td>$value[1]</td>
+            <td>$sallDemand[0]</td>
+            <td>$value[4]</td>
+            <td>$value[2]</td>
+            <td>$sallDemand[1]</td>
+            </tr>
+            only;
+
+            $income += $sallDemand[1];
+            $list .= $one;
+        }
+        $list .= "<tr><td colspan='5'>總營收: $income</td></tr>";
+        return $list;
+    }
+
+    public function image(){
+        $name = [];
+        $array = [];
+        
+
+        $image = <<<detail
+        <canvas id="myChart"  width="200" height="100"></canvas>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+        <script>
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'bar',
+
+                // The data for our dataset
+                data: {
+                    labels: $name,
+                    datasets: [{
+                        label: '本日銷售量',
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: $array
+                    }]
+                },
+
+                // Configuration options go here
+                options: {}
+            });
+        </script>
+        detail;
+        return $image;
+    }
+}
+
+?>
---
+++ MVCmod/models/masterPostModel.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once './models/database.php';
+session_start();
+
+class masterPostM extends database{
+
+    public $member;
+
+    public function __construct(){
+        if(!isset($_SESSION['mid'])){
+            header("location: ./hello");
+            exit();
+        }
+
+        $mid = $_SESSION["mid"];
+        $search = self::query("SELECT userName, grade FROM webMaster WHERE id = $mid");
+        $this->member = $search;
+    }
+
+    public function product(){
+        $search = self::query("SELECT * FROM product;");
+        return $search;
+    }
+
+    public function demand($day, $pid, $price){
+        $oneIncome = 0;
+        $sall = <<<sallserach
+        SELECT SUM(demand) demand FROM product p
+        JOIN orderDetail od ON od.productId = p.id
+        JOIN memberOrder mo ON od.orderId = mo.id
+        WHERE DATEDIFF(NOW(), orderTime) < $day AND p.id = $pid
+        sallserach;
+        $search = self::query($sall);
+        if(isset($search[0]['demand'])){
+            $sallDemand = $search[0]['demand'];
+        }else{
+            $sallDemand = 0;
+        }
+        $oneIncome = $sallDemand*$price;
+
+        $array = [$sallDemand, $oneIncome];
+        return $array;
+    }
+
+    public function demandDay($day1, $day2, $pid, $price){
+        $oneIncome = 0;
+        $sall = <<<sallserach
+        SELECT SUM(demand) demand FROM product p
+        JOIN orderDetail od ON od.productId = p.id
+        JOIN memberOrder mo ON od.orderId = mo.id
+        WHERE orderTime <= '$day2' AND orderTime >= '$day1' AND p.id = $pid
+        sallserach;
+        $search = self::query($sall);
+        if(isset($search[0]['demand'])){
+            $sallDemand = $search[0]['demand'];
+        }else{
+            $sallDemand = 0;
+        }
+        $oneIncome = $sallDemand*$price;
+
+        $array = [$sallDemand, $oneIncome];
+        return $array;
+    }
+
+}
+
+?>
