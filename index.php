2cb116089e224f4788819c974f5e8a3b1b6c467d
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-09-07 14:13:43 +0800

    index : debug

--- index.php
+++ index.php
@@ -48,7 +48,6 @@ if(isset($_GET["logout"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>三點半</title>
-
     <link rel="stylesheet" href="./CSS/indexStyle.css">
 
 </head>
@@ -162,9 +161,6 @@ if(isset($_GET["logout"])){
     </div>
 
     <!-- 測試區域 -->
-    <div id="test">
-        
-    </div>
 
     <!-- 聯絡我們 -->
     <footer>
